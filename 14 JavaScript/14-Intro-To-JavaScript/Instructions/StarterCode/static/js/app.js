4978666552472cc2de22fff5c204cc19307b45b2
Kaileigh Kinkoph <40719533+KNKinkoph@users.noreply.github.com>
2018-10-20 10:03:29 -0500

    JavaScript Homework

--- 14 JavaScript/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14 JavaScript/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,43 @@
+// Homework:
 // from data.js
 var tableData = data;
+var tablebody =  d3.select("tbody")
+// Appends data 
+// Add new row of data for each UFO sighting
+// Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+// Build the table out:
+function ufoTable(data){
+tablebody.html("") 
+data.forEach((datarow)=>{
+    var row = tablebody.append("tr")
+    Object.values(datarow).forEach((value)=>{
+        var cell = row.append("td")
+        cell.text(value)
+    })   
+})
+}
+ufoTable(tableData)
 
-// YOUR CODE HERE!
+// Filter table
+
+function filterTable(){
+d3.event.preventDefault()
+var date = d3.select("#datetime").property("value")
+var filterData = tableData
+if (date){
+    filterData = filterData.filter(row=>row.datetime===date)
+}
+ufoTable(filterData)
+}
+// On click for the event of filter
+d3.selectAll("#filter-btn").on("click", filterTable)
+
+
+// Kaileigh Notes:
+// Part 2: Drop down, in each option if/else if statment 
+// city = value
+// d3.event.preventDefault()
+// var date = d3.select("#datetime").property("value")
+// var filterData = tableData
+// if (date){
+//     filterData = filterData.filter(row=>row.datetime===date)
